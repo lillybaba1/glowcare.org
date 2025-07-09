d9b28107173934cb7a8dbb93bb00675f0685c9b0
Heilige Geist <heiligegeist01@gmail.com>
2025-07-09 17:42:15 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,6 @@
 import type {NextConfig} from 'next';
 
 const nextConfig: NextConfig = {
-  output: 'export',
   allowedDevOrigins: [
     '*.cloudworkstations.dev',
     '6000-firebase-studio-1751794696136.cluster-oayqgyglpfgseqclbygurw4xd4.cloudworkstations.dev',
